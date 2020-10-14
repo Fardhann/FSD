bd0ed630a8c93c2e40f7503017d6dfe9578c419b
Fardhann <72858292+Fardhann@users.noreply.github.com>
2020-10-14 20:33:28 +0700

    Add files via upload

---
+++ Soal Probabilitas.R
@@ -0,0 +1,11 @@
+#6
+Seorang petugas ingin menghitung probabilitas untuk
+mendapatkan 4 bola lampu yang rusak dari suatu
+sampel acak sebanyak 20 bola lampu, apabila
+diketahui bahwa 10 % dari bola lampu tersebut
+rusak. Nilai probabilitas ini dapat diperoleh dari tabel
+binomial yang dibuat berdasarkan distribusi
+binomial
+
+dbinom(x=4, size=20, prob=0.1)
+
